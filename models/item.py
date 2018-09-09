5c326f00503648e46d9fd6da286713a0b8e84efd
Lou <lou@Lous-iMac.home>
2018-09-09 17:09:42 -0400

    add sqlachemy to models

--- models/item.py
+++ models/item.py
@@ -1,8 +1,15 @@
 from decorators import db_check_or_return_500
 import sqlite3
+from db import db
 
 
-class ItemModel:
+class ItemModel(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    price = db.Column(db.Float(precision=2))
+
     def __init__(self, name, price):
         self.name = name
         self.price = price
